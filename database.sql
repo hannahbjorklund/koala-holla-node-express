831e3c6c60b3d220c3ab2edef0be17f3945a8607
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-09 10:19:50 -0600

    Setup pool in pool.js and made our database

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "koalas"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(100),
+	"gender" VARCHAR(10),
+	"age" INTEGER,
+	"ready_to_transfer" BOOLEAN,
+	"notes" VARCHAR(500)
+	);
+	
+INSERT INTO "koalas"
+	("name", "gender", "age", "ready_to_transfer", "notes")
+	VALUES
+	('Scotty', 'M', 4, true, 'Born in Guatemala'),
+	('Jean', 'F', 5, true, 'Allergic to lots of lava'),
+	('Ororo', 'F', 7, false, 'Loves listening to Paula (Abdul)'),
+	('K''leaf', 'NB', 15, false, 'Never refuses a treat'),
+	('Charlie', 'M', 9, true, 'Favorite band is Nirvana'),
+	('Betsy', 'F', 4, true, 'Has a pet iguana');
