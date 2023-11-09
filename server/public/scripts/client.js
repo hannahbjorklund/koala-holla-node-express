26c34ec4cc3e3ff2b92115f38bb644293ef578b1
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-09 10:34:31 -0600

    Created getKoalas, which makes a GET request to the server. Also made a GET route so the server can get koalas from the DB

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,16 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  }).then((response) => {
+    let koalas = response.data;
+    console.log("GET request at /koalas, response.data:", koalas);
+    renderKoalas(koalas);
+  }).catch((error) => {
+    console.error("Error in GET", error);
+  })
   
 } // end getKoalas
 
@@ -10,6 +20,10 @@ function saveKoala(){
   console.log( 'in saveKoala' );
   // axios call to server to get koalas
  
+}
+
+function renderKoalas(koalas){
+  console.log(' in renderKoalas', koalas);
 }
 
 getKoalas();
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,7 +5,19 @@ const koalaRouter = express.Router();
 const pool = require('../modules/pool');
 
 // GET
+koalaRouter.get('/', (req, res) => {
+    const sqlQueryText = `
+    SELECT * FROM "koalas"
+    `
 
+    pool.query(sqlQueryText)
+    .then((dbResult) => {
+        console.log("Retrieved koalas from DB", dbResult.rows);
+        res.send(dbResult.rows);
+    }).catch((dbError) => {
+        console.error("Could not retrieve koalas. Relinquish.", dbError);
+    })
+})
 
 // POST
 
