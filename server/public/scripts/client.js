4b08e30ae7929a5b782e1ff320b0a70f56353374
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-09 10:43:19 -0600

    Created renderKoalas to display koalas on the DOM and made the icon smaller in css

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -24,6 +24,22 @@ function saveKoala(){
 
 function renderKoalas(koalas){
   console.log(' in renderKoalas', koalas);
+  // Get the HTML element where the koalas will live
+  let koalaEnclosure = document.getElementById('viewKoalas');
+  koalaEnclosure.innerHTML = '';
+
+  // Loop through the koalas and display each one
+  for(let koala of koalas){
+    koalaEnclosure.innerHTML += `
+    <tr>
+      <td>${koala.name}</td>
+      <td>${koala.gender}</td>
+      <td>${koala.age}</td>
+      <td>${koala.ready_to_transfer}</td>
+      <td>${koala.notes}</td>
+    </tr>
+    `
+  }
 }
 
 getKoalas();
